0b1112afa76faca39faa82230abf85cbdbb51272
MichaelPeriod <39890684+MichaelPeriod@users.noreply.github.com>
2021-06-11 16:54:07 -0400

    Added cutting call

--- GMTK GameJam - Infinity Brew/Assets/Scripts/PlayerController.cs
+++ GMTK GameJam - Infinity Brew/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D RB;
     public float playerSpeed;
     public int playerNumber;
+    public float cutDistance = 1f;
+    public GameObject otherPlayer;
     // Start is called before the first frame update
     void Start(){
         RB = gameObject.GetComponent<Rigidbody2D>();
@@ -14,11 +16,13 @@ public class PlayerController : MonoBehaviour
 
     void Update(){
         //Move it based on if pressing, dev set speed, and time since last frame
-	if (playerNumber == 1) {
-        	RB.AddForce(new Vector2(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime * 100));
-	}
-	else {
-		RB.AddForce(new Vector2(Input.GetAxis("H2") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("V2") * playerSpeed * Time.deltaTime * 100));
+        if (playerNumber == 1) {
+            RB.AddForce(new Vector2(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime * 100));
+            if(Vector2.Distance(gameObject.GetComponent<Transform>().position, otherPlayer.GetComponent<Transform>().position) < cutDistance){
+                Debug.Log("Cut");
+            }
+        } else {
+            RB.AddForce(new Vector2(Input.GetAxis("H2") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("V2") * playerSpeed * Time.deltaTime * 100));
         }
     }
 }
