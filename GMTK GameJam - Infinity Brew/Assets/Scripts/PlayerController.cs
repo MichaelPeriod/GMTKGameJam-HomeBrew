5db0e16c88fbdcbfee53727c7ad15c1db39c48b4
TobinTojo <tobintojo@gmail.com>
2021-06-11 15:48:07 -0400

    Player movement for both players
    Created movement for player 1 and player 2

--- GMTK GameJam - Infinity Brew/Assets/Scripts/PlayerController.cs
+++ GMTK GameJam - Infinity Brew/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D RB;
     public float playerSpeed;
+    public int playerNumber;
     // Start is called before the first frame update
     void Start(){
         RB = gameObject.GetComponent<Rigidbody2D>();
@@ -13,6 +14,11 @@ public class PlayerController : MonoBehaviour
 
     void Update(){
         //Move it based on if pressing, dev set speed, and time since last frame
-        RB.AddForce(new Vector2(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime * 100));
+	if (playerNumber == 1) {
+        	RB.AddForce(new Vector2(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime * 100));
+	}
+	else {
+		RB.AddForce(new Vector2(Input.GetAxis("H2") * playerSpeed * Time.deltaTime * 100, Input.GetAxis("V2") * playerSpeed * Time.deltaTime * 100));
+        }
     }
 }
